0eac04111937df765451c730de4a56651da495a3
Noah Groleau <98351050+NoahGJAC@users.noreply.github.com>
2023-11-23 19:47:09 -0500

    Update WorkoutDestinations.kt
    removed val screen from interface and objects

--- app/src/main/java/com/example/workouttracker/WorkoutDestinations.kt
+++ app/src/main/java/com/example/workouttracker/WorkoutDestinations.kt
@@ -7,23 +7,18 @@ import com.example.workouttracker.exerciselist.WorkoutList
 
 interface WorkoutDestination {
     val route: String
-    val screen: @Composable () -> Unit
 }
 
 object ExerciseInput:WorkoutDestination{
     override val route = "exerciseinput"
-    override val screen: @Composable () -> Unit = { WorkoutInput() }
 }
 object ExerciseList: WorkoutDestination{
     override val route = "exerciselist"
-    override val screen: @Composable () -> Unit = { WorkoutList() }
 
 }
 
 object Notes: WorkoutDestination{
     override val route = "notes"
-    override val screen: @Composable () -> Unit = { ToDoScreen() }
-
 }
 
 val exerciseTabRowScreens = listOf(ExerciseInput, ExerciseList, Notes)
