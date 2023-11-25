60c9ff37d484172b2008fe28827a852a04284980
Alejandro <2168579@johnabbottcollege.net>
2023-11-24 19:41:49 -0500

    finished base calendat

--- app/src/main/java/com/example/workouttracker/calendar/MainActivity.kt
+++ app/src/main/java/com/example/workouttracker/calendar/MainActivity.kt
@@ -1,22 +1,23 @@
-package com.example.workouttracker.calendar
-
-
+package com.example.calendartest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,15 +27,74 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.workouttracker.R
+import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.YearMonth
 import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.time.temporal.ChronoUnit
+import java.util.stream.Collectors
+import java.util.stream.Stream
 
+data class CalendarUiModel(
+    val selectedDate: Date, // the date selected by the User. by default is Today.
+    val visibleDates: List<Date> // the dates shown on the screen
+) {
+
+    val startDate: Date = visibleDates.first() // the first of the visible dates
+    val endDate: Date = visibleDates.last() // the last of the visible dates
+
+    data class Date(
+        val date: LocalDate,
+        val isSelected: Boolean,
+        val isToday: Boolean
+    ) {
+        val day: String = date.format(DateTimeFormatter.ofPattern("E")) // get the day by formatting the date
+    }
+}
+class CalendarDataSource {
+
+    val today: LocalDate
+        get() {
+            return LocalDate.now()
+        }
+
+
+    fun getData(startDate: LocalDate = today, lastSelectedDate: LocalDate): CalendarUiModel {
+        val firstDayOfWeek = startDate.with(DayOfWeek.MONDAY)
+        val endDayOfWeek = firstDayOfWeek.plusDays(7)
+        val visibleDates = getDatesBetween(firstDayOfWeek, endDayOfWeek)
+        return toUiModel(visibleDates, lastSelectedDate)
+    }
+
+    private fun getDatesBetween(startDate: LocalDate, endDate: LocalDate): List<LocalDate> {
+        val numOfDays = ChronoUnit.DAYS.between(startDate, endDate)
+        return Stream.iterate(startDate) { date ->
+            date.plusDays(/* daysToAdd = */ 1)
+        }
+            .limit(numOfDays)
+            .collect(Collectors.toList())
+    }
+
+    private fun toUiModel(
+        dateList: List<LocalDate>,
+        lastSelectedDate: LocalDate
+    ): CalendarUiModel {
+        return CalendarUiModel(
+            selectedDate = toItemUiModel(lastSelectedDate, true),
+            visibleDates = dateList.map {
+                toItemUiModel(it, it.isEqual(lastSelectedDate))
+            },
+        )
+    }
+
+    private fun toItemUiModel(date: LocalDate, isSelectedDate: Boolean) = CalendarUiModel.Date(
+        isSelected = isSelectedDate,
+        isToday = date.isEqual(today),
+        date = date,
+    )
+}
 class CalendarActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,91 +106,123 @@ class CalendarActivity : ComponentActivity() {
 
 @Composable
 fun CalendarContent() {
-    var selectedDate by remember { mutableStateOf(LocalDate.now()) }
-
+    val dataSource = CalendarDataSource()
+    var calendarUiModel by remember { mutableStateOf(dataSource.getData(lastSelectedDate = dataSource.today)) }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
         // Month and year header
-        CalendarHeader(selectedDate = selectedDate)
+        Header(data=calendarUiModel,onPrevClickListener = { startDate ->
+            // refresh the CalendarUiModel with new data
+            // by get data with new Start Date (which is the startDate-1 from the visibleDates)
+            val finalStartDate = startDate.minusDays(1)
+            calendarUiModel = dataSource.getData(startDate = finalStartDate, lastSelectedDate = calendarUiModel.selectedDate.date)
+        },
+            onNextClickListener = { endDate ->
+                // refresh the CalendarUiModel with new data
+                // by get data with new Start Date (which is the endDate+2 from the visibleDates)
+                val finalStartDate = endDate.plusDays(2)
+                calendarUiModel = dataSource.getData(startDate = finalStartDate, lastSelectedDate = calendarUiModel.selectedDate.date)
+            })
+        Content(data = calendarUiModel, onDateClickListener = { date ->
+            // refresh the CalendarUiModel with new data
+            // by changing only the `selectedDate` with the date selected by User
+            calendarUiModel = calendarUiModel.copy(
+                selectedDate = date,
+                visibleDates = calendarUiModel.visibleDates.map {
+                    it.copy(
+                        isSelected = it.date.isEqual(date.date)
+                    )
+                }
+            )
+        })
 
-        // Calendar days
-        CalendarGrid(selectedDate = selectedDate) { selectedDate = it }
     }
 }
-
-
 @Composable
-fun CalendarHeader(selectedDate: LocalDate) {
-    val formatter = DateTimeFormatter.ofPattern("MMMM yyyy")
-    val formattedDate = selectedDate.format(formatter)
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 8.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+fun Header(data: CalendarUiModel,
+           onPrevClickListener: (LocalDate) -> Unit,
+           onNextClickListener: (LocalDate) -> Unit,) {
+    Row {
         Text(
-            text = formattedDate,
-            style = MaterialTheme.typography.bodyLarge
+            // show "Today" if user selects today's date
+            // else, show the full format of the date
+            text = if (data.selectedDate.isToday) {
+                "Today"
+            } else {
+                data.selectedDate.date.format(
+                    DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+                )
+            },
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
         )
-    }
-}
-
-@Composable
-fun CalendarGrid(selectedDate: LocalDate, onDateClick: (LocalDate) -> Unit) {
-    val daysInMonth = YearMonth.from(selectedDate).lengthOfMonth()
-    val firstDayOfMonth = LocalDate.of(selectedDate.year, selectedDate.month, 1)
-    val leadingEmptyDays = firstDayOfMonth.dayOfWeek.value - 1
-
-    LazyColumn {
-        items(items = (0 until leadingEmptyDays).toList()) {
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(1f)
-                    .background(MaterialTheme.colorScheme.background)
+        IconButton(onClick = { onPrevClickListener(data.startDate.date) }) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = "Back"
             )
         }
-
-        items(items = (1..daysInMonth).toList()) { day ->
-            val date = LocalDate.of(selectedDate.year, selectedDate.month, day)
-
-            DayItem(
-                date = date,
-                isSelected = date == selectedDate,
-                onDateClick = onDateClick
+        IconButton(onClick = {onNextClickListener(data.endDate.date) }) {
+            Icon(
+                imageVector = Icons.Filled.ArrowForward,
+                contentDescription = "Next"
             )
         }
     }
 }
 
-
-
-
 @Composable
-fun DayItem(date: LocalDate, isSelected: Boolean, onDateClick: (LocalDate) -> Unit) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .aspectRatio(1f)
-            .background(if (isSelected) colorResource(id = R.color.purple_500) else Color.Transparent)
-            .clickable { onDateClick(date) },
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = date.dayOfMonth.toString(),
-            style = MaterialTheme.typography.bodyLarge.copy(
-                color = if (isSelected) Color.White else MaterialTheme.colorScheme.onSurface
-            )
-        )
+fun Content(data: CalendarUiModel,
+            onDateClickListener: (CalendarUiModel.Date) -> Unit,) {
+    LazyRow {
+        // pass the visibleDates to the UI
+        items(items = data.visibleDates) { date ->
+            ContentItem(date,onDateClickListener)
+        }
     }
 }
 
+@Composable
+fun ContentItem(date: CalendarUiModel.Date,
+                onClickListener: (CalendarUiModel.Date) -> Unit,) {
+    Card(
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 4.dp)
+            .clickable{ onClickListener(date)}
+        ,
+        colors = CardDefaults.cardColors(
+            // background colors of the selected date
+            // and the non-selected date are different
+            containerColor = if (date.isSelected) {
+                MaterialTheme.colorScheme.primary
+            } else {
+                MaterialTheme.colorScheme.secondary
+            }
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .width(40.dp)
+                .height(48.dp)
+                .padding(4.dp)
+        ) {
+            Text(
+                text = date.day, // day "Mon", "Tue"
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.bodySmall
+            )
+            Text(
+                text = date.date.dayOfMonth.toString(), // date "15", "16"
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun CalendarPreview() {
