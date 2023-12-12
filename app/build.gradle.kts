33db23c409a4022a53ab8ff22a0f37880244de1a
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2023-12-12 18:39:53 -0500

    What iff i blew my brains out

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,9 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-auth")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("com.google.android.material:material:1.5.0")
@@ -76,5 +77,4 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
-
 }
