408386b2eab52fb50a8d90ca8afb0839bd66da8e
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2023-12-12 22:32:54 -0500

    Overall Code Improvements + Addition of documentation
    Updated to use a List instead of concatenating a string + UI overhaul of displaying the list. Documentation added.

--- app/src/main/java/com/example/gymplanner/exerciseinput/ExerciseInput.kt
+++ app/src/main/java/com/example/gymplanner/exerciseinput/ExerciseInput.kt
@@ -21,32 +21,48 @@ import com.google.firebase.firestore.FirebaseFirestore
  *
  * @see [https://developer.android.com/courses/pathways/jetpack-compose-for-android-developers-1]
  */
+
+/**
+ * Composable function for adding exercises to the Workout Tracker App.
+ *
+ * This composable provides a simple form for users to input exercise details such as
+ * exercise name, number of reps, and weight. Users can add exercises, and the added
+ * exercises are displayed in a list below the input form.
+ *
+ * @param db Firebase Firestore instance for storing exercise data.
+ */
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun WorkoutInput(db: FirebaseFirestore) {
-    // used to hide the keyboard when add exercise button is clicked
+    // Used to hide the keyboard when the "Add Exercise" button is clicked.
     // @see [https://stackoverflow.com/questions/69124822/android-compose-show-and-hide-keyboard]
     val keyboard = LocalSoftwareKeyboardController.current
 
+    // State variables to store input values and the list of exercises.
     var exerciseName by rememberSaveable { mutableStateOf("") }
     var exerciseReps by rememberSaveable { mutableStateOf("") }
     var exerciseWeight by rememberSaveable { mutableStateOf("") }
-    var exercisesList by rememberSaveable { mutableStateOf("") }
+    var exercisesList by rememberSaveable { mutableStateOf(emptyList<String>()) }
 
+
+    // Scaffold provides the basic structure of the screen, including the app bar.
     Scaffold(
         contentColor = MaterialTheme.colorScheme.primary
     ) {
+        // Box is used to center the content on the screen.
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it), contentAlignment = Alignment.Center
         ) {
+            // Column is a vertical arrangement of composables.
             Column(
                 modifier = Modifier.padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                // Input fields for exercise details.
                 OutlinedTextField(
                     value = exerciseName,
                     onValueChange = { exerciseName = it },
@@ -71,10 +87,11 @@ fun WorkoutInput(db: FirebaseFirestore) {
                     modifier = Modifier.semantics { contentDescription = "Weight" }
                 )
 
+                // Button to add an exercise.
                 Button(
                     onClick = {
                         if (exerciseName.isNotEmpty() && exerciseReps.isNotEmpty() && exerciseWeight.isNotEmpty()) {
-                            exercisesList += "$exerciseName - $exerciseReps reps - $exerciseWeight lbs\n"
+                            exercisesList = exercisesList + "$exerciseName - $exerciseReps reps - $exerciseWeight lbs"
                             exerciseName = ""
                             exerciseReps = ""
                             exerciseWeight = ""
@@ -90,11 +107,32 @@ fun WorkoutInput(db: FirebaseFirestore) {
                 }
 
                 LazyColumn {
-                    items(exercisesList.split("\n")) { exercise ->
-                        Text(
-                            exercise,
-                            modifier = Modifier.semantics { contentDescription = "Exercise: $exercise" }
-                        )
+                    items(exercisesList) { exercise ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            colors = CardDefaults.cardColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                contentColor = MaterialTheme.colorScheme.onBackground
+                            ),
+                            shape = MaterialTheme.shapes.medium
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                // Exercise Name
+                                Text(
+                                    exercise,
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .padding(end = 16.dp).semantics { contentDescription = "Exercise: $exercise" }
+                                )
+                            }
+                        }
                     }
                 }
             }
