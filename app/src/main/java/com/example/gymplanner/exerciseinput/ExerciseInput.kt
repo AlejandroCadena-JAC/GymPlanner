1a572f6dd063061838917cb264c8c61fcd19ad2e
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2023-12-12 22:09:27 -0500

    Exercise Input Accessibility Improvements

--- app/src/main/java/com/example/gymplanner/exerciseinput/ExerciseInput.kt
+++ app/src/main/java/com/example/gymplanner/exerciseinput/ExerciseInput.kt
@@ -1,27 +1,20 @@
 package com.example.gymplanner.exerciseinput
 
 import android.annotation.SuppressLint
-import androidx.compose.runtime.*
-import androidx.compose.material.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.FirebaseFirestore
-
-
-
 
 /**
  * Main composable function for the Workout Tracker App.
@@ -41,14 +34,14 @@ fun WorkoutInput(db: FirebaseFirestore) {
     var exerciseWeight by rememberSaveable { mutableStateOf("") }
     var exercisesList by rememberSaveable { mutableStateOf("") }
 
-
     Scaffold(
         contentColor = MaterialTheme.colorScheme.primary
-    ){
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(it), contentAlignment = Alignment.Center)
-        {
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it), contentAlignment = Alignment.Center
+        ) {
             Column(
                 modifier = Modifier.padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -58,50 +51,53 @@ fun WorkoutInput(db: FirebaseFirestore) {
                     value = exerciseName,
                     onValueChange = { exerciseName = it },
                     label = { Text("Exercise Name") },
-                    singleLine = true
+                    singleLine = true,
+                    modifier = Modifier.semantics { contentDescription = "Exercise Name" }
                 )
 
                 OutlinedTextField(
                     value = exerciseReps,
                     onValueChange = { exerciseReps = it },
                     label = { Text("Reps") },
-                    singleLine = true
+                    singleLine = true,
+                    modifier = Modifier.semantics { contentDescription = "Reps" }
                 )
 
                 OutlinedTextField(
                     value = exerciseWeight,
                     onValueChange = { exerciseWeight = it },
                     label = { Text("Weight") },
-                    singleLine = true
+                    singleLine = true,
+                    modifier = Modifier.semantics { contentDescription = "Weight" }
                 )
 
-                Button(onClick = {
-                    if (exerciseName.isNotEmpty() && exerciseReps.isNotEmpty() && exerciseWeight.isNotEmpty()) {
-                        exercisesList += "$exerciseName - $exerciseReps reps - $exerciseWeight lbs\n"
-                        exerciseName = ""
-                        exerciseReps = ""
-                        exerciseWeight = ""
-                    }
-                    keyboard?.hide()
-                },
-                    modifier = Modifier.size(width = 130.dp, height = 58.dp).
-                    padding(top = 12.dp)
-
-
+                Button(
+                    onClick = {
+                        if (exerciseName.isNotEmpty() && exerciseReps.isNotEmpty() && exerciseWeight.isNotEmpty()) {
+                            exercisesList += "$exerciseName - $exerciseReps reps - $exerciseWeight lbs\n"
+                            exerciseName = ""
+                            exerciseReps = ""
+                            exerciseWeight = ""
+                        }
+                        keyboard?.hide()
+                    },
+                    modifier = Modifier
+                        .size(width = 100.dp, height = 58.dp)
+                        .padding(top = 12.dp)
+                        .semantics { contentDescription = "Add Exercise Button" }
                 ) {
-                    Text("Add Exercise")
+                    Text("Add")
                 }
 
                 LazyColumn {
                     items(exercisesList.split("\n")) { exercise ->
-                        Text(exercise)
+                        Text(
+                            exercise,
+                            modifier = Modifier.semantics { contentDescription = "Exercise: $exercise" }
+                        )
                     }
                 }
             }
         }
     }
 }
-
-
-
-
